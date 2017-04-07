ba0b76ae52b25094112c9bb0ca02578de28ff4e6
Taylor Hudson <theshrewedshrew@gmail.com>
2017-04-07 10:31:08 -0500

    fix

--- src/driver.java
+++ src/driver.java
@@ -3,20 +3,48 @@
  */
 public class driver {
     //static int count = 0;
-    public static void main(String [] args0){
-        LoginBox lb = new LoginBox();
-        submit scoreSlaughter[] = new submit[16];
-        while(!lb.access()){
-            try{
-                Thread.sleep(19);
+    static int sleeptime = 15; /// Time in seconds
+    static final int number = 999;
+    static final int numLoginBox = 1;
+
+    public static void main(String[] args0) {
+
+        LoginBox lb[];
+        lb = new LoginBox[numLoginBox];
+        for (int i = 0; i < numLoginBox; i++)
+            lb[i] = new LoginBox();
+
+        submit scoreSlaughter[] = new submit[number];
+        if (numLoginBox > 1)
+            for (int i = 0; i < number; i++)
+                while (!lb[number].access()) {
+                    try {
+                        Thread.sleep(19);
+                    } catch (Exception e) {
+
+                    }
+                }
+        else {
+            while (!lb[0].access()) {
+                try {
+                    Thread.sleep(19);
+                } catch (Exception e) {
+
+                }
+
             }
-            catch(Exception e){
+            if (numLoginBox < 2) {
+                for (int i = 0; i < number; i++)
+                    scoreSlaughter[i] = new submit(lb[0].getUsername(), lb[0].getPassword());
+                for (int i = 0; i < number; i++)
+                    new Thread(scoreSlaughter[i]).start();
+            } else {
+                for (int i = 0; i < number; i++)
+                    scoreSlaughter[i] = new submit(lb[number].getUsername(), lb[number].getPassword());
+                for (int i = 0; i < number; i++)
+                    new Thread(scoreSlaughter[0]).start();
 
             }
         }
-        for(int i = 0; i < 16; i++)
-            scoreSlaughter[i] = new submit(lb.getUsername(), lb.getPassword());
-        for(int i = 0; i < 16; i ++)
-            new Thread(scoreSlaughter[i]).start();
     }
 }
