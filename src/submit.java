17f6c0bafe9fc48077689036605dfc8df70c67d6
Taylor Hudson <theshrewedshrew@gmail.com>
2017-04-07 10:31:50 -0500

    update

--- src/submit.java
+++ src/submit.java
@@ -15,20 +15,26 @@ public class submit implements Runnable {
     @Override
     public void run() {
         while (true) {
-            int fifty = (int) (Math.random() * 50000)*10;
-            int sizeX = (int) (Math.random() * 21) + 5;
-            int sizeY = sizeX;
+
+            int sizeX = (int) (Math.random() * 100000) + 5;
+            if(user.equals("qq")){ sizeX = 25; }
+            int sizeY = sizeX;//(int) (Math.random() * 96) + 5;
+            int fifty = (int) (Math.random() * 5000000)*10+10;
             double time = (double) (fifty) / 1000;
             URL url;
             try {
                     url = new URL("http://westonreed.com/picross/addscore.php?username=" + user + "&password=" + pass + "&time=" + time + "&size=" + sizeX + "x" + sizeY + "&version=" + VERSION);
+
                     url.openStream();
                     //System.out.println(driver.count++);
                 } catch (Exception ex) {
                 }
 
             try{
-                Thread.sleep(1500);
+                if(fifty > 50000 && !user.equals("qq"))
+                    Thread.sleep((int)time);
+                else
+                    Thread.sleep((int)(Math.random()*98768) + 124132);
             }catch(Exception e){
 
             }
